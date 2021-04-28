c54db652c17aae76d81c2d4026a94f848cefdd9e
manishgupta577 <67793046+manishgupta577@users.noreply.github.com>
2021-04-28 05:53:09 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $hostname = "http://sql6.freemysqlhosting.net/news-site";
 
-$conn = mysqli_connect("sql6.freemysqlhosting.net","	sql6408810","1nrhRUmeDw","	sql6408810") or die("Connection failed : " . mysqli_connect_error());
+$conn = mysqli_connect("sql6.freemysqlhosting.net","sql6408810","1nrhRUmeDw","sql6408810") or die("Connection failed : " . mysqli_connect_error());
 
 ?>
